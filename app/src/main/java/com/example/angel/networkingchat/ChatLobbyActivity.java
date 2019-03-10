0209ba17116cff4d24b213a0a11475df9647ebce
Angel <sigma271@gmail.com>
2019-03-09 19:05:22 -0600

    My first fragment is working! :D

--- app/src/main/java/com/example/angel/networkingchat/ChatLobbyActivity.java
+++ app/src/main/java/com/example/angel/networkingchat/ChatLobbyActivity.java
@@ -1,11 +1,19 @@
 package com.example.angel.networkingchat;
 
+import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Toast;
 import android.os.Handler;
 
+import com.example.angel.networkingchat.fragment.ActivePeopleFragment;
+import com.example.angel.networkingchat.fragment.ForumFragment;
+import com.example.angel.networkingchat.fragment.PrivateMessageFragment;
 import com.example.angel.networkingchat.utilidades.Const;
 import com.example.angel.networkingchat.utilidades.MyMessage;
 import com.example.angel.networkingchat.utilidades.UtilFun;
@@ -29,16 +37,51 @@ public class ChatLobbyActivity extends AppCompatActivity {
         Intent intent = getIntent();
         username = intent.getStringExtra(MainActivity.USERNAME);
         Toast.makeText(this, username, Toast.LENGTH_SHORT).show();
-        new Thread(new OwnServer()).start();
+        new Thread(new OwnServer(getApplicationContext())).start();
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
 
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment fragmentSelected = null;
+
+                    switch (menuItem.getItemId()) {
+                        case R.id.nav_private_chat:
+                            fragmentSelected = new PrivateMessageFragment(); break;
+                        case R.id.nav_forum:
+                            fragmentSelected = new ForumFragment(); break;
+                        case R.id.nav_people_available:
+                            fragmentSelected = new ActivePeopleFragment(); break;
+                        // avoid the posibility of being null
+                        default: fragmentSelected = new ForumFragment(); break;
+                    }
+
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment_container, fragmentSelected)
+                            .commit();
+
+                    return true;
+                }
+            };
+
+    // Nested class ============================
     private class OwnServer implements Runnable {
         private MulticastSocket socket;
         private byte[] buf = new byte[Const.MAX_UDP_LENGTH];
+        private Context context;
 
         // We cannot modify UI components directly if we are not the main thread
         // I'll use handler in this example
         private Handler handler = new Handler();
+
+        public OwnServer(Context context) {
+            this.context = context;
+        }
 
         public void run() {
             try {
@@ -54,7 +97,7 @@ public class ChatLobbyActivity extends AppCompatActivity {
                     handler.post(new Runnable() {
                         @Override
                         public void run() {
-                            Toast.makeText(getApplicationContext(), msg.toString(), Toast.LENGTH_LONG).show();
+                            Toast.makeText(context, msg.toString(), Toast.LENGTH_LONG).show();
                         }
                     });
 
---
+++ app/src/main/java/com/example/angel/networkingchat/fragment/ActivePeopleFragment.java
@@ -0,0 +1,19 @@
+package com.example.angel.networkingchat.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.angel.networkingchat.R;
+
+public class ActivePeopleFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_active_people, container, false);
+    }
+}
