9a1fbc30c096049894b20b6beb36282fe23e32ce
Angel <sigma271@gmail.com>
2019-03-10 21:10:10 -0600

    Some resources for RecyclerView

--- app/src/main/java/com/example/angel/networkingchat/recyclerViewAvailablePeople/PersonAvailableItem.java
+++ app/src/main/java/com/example/angel/networkingchat/recyclerViewAvailablePeople/PersonAvailableItem.java
@@ -1,5 +1,7 @@
 package com.example.angel.networkingchat.recyclerViewAvailablePeople;
 
 public class PersonAvailableItem {
+    public PersonAvailableItem(int img, String nick, String addr) {
 
+    }
 }
